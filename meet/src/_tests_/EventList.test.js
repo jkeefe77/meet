7b0e7178ce9e82b6c245310a4cee2434818478df
jkeefe77 <jvkeefe@gmail.com>
2023-08-08 17:37:05 -0400

    Revert "Revert "test""
    This reverts commit 1711353536cd6cc4a1492575f7371bce9680a7fd.

---
+++ meet/src/_tests_/EventList.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import EventList from "../components/EventList";
+import { getEvents } from "../api";
+
+describe("<EventList /> component", () => {
+  const setup = (events = []) => {
+    render(<EventList events={events} />);
+  };
+
+  test('has an element with "list" role', () => {
+    setup();
+    expect(screen.getByRole("list")).toBeInTheDocument();
+  });
+
+  test("renders correct number of events", async () => {
+    const allEvents = await getEvents();
+    setup(allEvents);
+    expect(screen.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  });
+});
